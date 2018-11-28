3b4778c70b961b4f44e4cd88851c00ad8e1b167b
Eduardo Cea <edu.cea@alumnos.duoc.cl>
2018-11-28 01:08:48 -0300

    Api instalada

--- FormularioPerris/views.py
+++ FormularioPerris/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+import requests
 from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    response = requests.get('http://api.ipstack.com/200.14.248.139?access_key=ed45289eb6aa84378442d6c312320945')
+    data = response.json()
+    return render(request,'index.html',{'latitud': data['latitude'],'longitud': data['longitude'],'ciudad': data['city'],'region': data['region_code'],'ip': data['ip'], 'pais': data['country_name'] })
