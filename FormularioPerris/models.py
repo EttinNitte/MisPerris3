a73c7d6c35bda585416a03c013931e173052e038
Eduardo Cea <edu.cea@alumnos.duoc.cl>
2018-11-27 21:20:07 -0300

    Modelo adoptante creado

--- FormularioPerris/models.py
+++ FormularioPerris/models.py
@@ -11,3 +11,14 @@ class Rescatado(models.Model):
     fechaRescate = models.DateField()
     op=(('R', 'Rescatado'),('D','Disponible'),('A','Adoptado'))
     estado= models.CharField(max_length=1, choices=op,default='R')
+
+class Adoptante(models.Model):
+    correo = models.CharField(max_length=60)
+    run =  models.CharField(max_length=11)
+    nombreCompleto = models.CharField(max_length=90)
+    pefechaNacimiento = models.DateField()
+    telefono = models.CharField(max_length=12)
+    region = models.CharField(max_length=30)
+    comuna = models.CharField(max_length=30)
+    op=(('G', 'Casa con patio grande'),('P','Casa con patio pequeño'),('S','Casa sin patio'),('D','Departamento'))
+    tipoVivienda= models.CharField(max_length=1, choices=op,default='G')
