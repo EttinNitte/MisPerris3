3b117ed44338c6248f212707bd91e44fd0f4c770
Samuel <s.devia@alumnos.duoc.cl>
2018-12-01 12:34:30 -0300

    html listo

--- FormularioPerris/views.py
+++ FormularioPerris/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 import requests
 from django.shortcuts import render
+from  .models import Adoptante
 
 # Create your views here.
 
 def index(request):
     response = requests.get('http://api.ipstack.com/200.14.248.139?access_key=ed45289eb6aa84378442d6c312320945')
     data = response.json()
+    #form = Adoptante (request.POST or None)
     return render(request,'index.html',{'latitud': data['latitude'],'longitud': data['longitude'],'ciudad': data['city'],'region': data['region_code'],'ip': data['ip'], 'pais': data['country_name'] })
